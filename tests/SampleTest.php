97d6dee476c4e368d15e12c881fc3ea341e828b7
Micheal Ataklt <matakltm.code@gmail.com>
2025-02-17 08:31:04 +0300

    Add sample test to verify PHPUnit setup

---
+++ tests/SampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chapa\Chapa\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testBasicAssertion()
+    {
+        // A simple test to verify that the testing environment is set up.
+        $this->assertTrue(true);
+    }
+}
